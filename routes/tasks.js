918413faea3b7d91d061a5c5af1fc9e7710f7fce
Noel David <noelsdavid@gmail.com>
2017-01-06 13:22:00 +0800

    mlab connection

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var router = express.Router();
+var mongojs = require('mongojs');
+
+var db = mongojs('mongodb://noeld:m123456@ds163397.mlab.com:63397/dbnoel',['tasks']);
 
 router.get('/tasks',function(req,res,next){
-    res.send('TAKS APIs');
+    // res.send('TAKS APIs');
+    db.tasks.find(function(err,tasks){
+    	if(err){
+    		res.send(err);
+    	}
+    	res.json(tasks);
+    });
 });
 
-module.exports = router;
+module.exports = router; 
