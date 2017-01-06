b6bd24f2dea1114670f74453ad76df922bb327ca
NOEL S. DAVID <noelsdavid@gmail.com>
2017-01-06 07:51:06 +0000

    angular files

---
+++ client/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+// import {TaskService} from './services/task.service';
+
+@Component({
+  // moduleId: module.id,
+  selector: 'my-app',
+  template: '<h1>My First Angular Application</h1>'
+  // providers:[TaskService]
+})
+
+export class AppComponent { }
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var tasks = require("./routes/tasks");
 
 var app = express();
 
-app.set('port', process.env.PORT);
+app.set('port', process.env.PORT || 3000);
 var port = app.get('port');
 
 //view engine
